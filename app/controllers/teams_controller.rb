b25550e02edb5486ef2286ffa99a90b37215b841
Katrina Pritchard <pritckat@gmail.com>
2018-09-19 14:44:41 +0000

    add teams/new.erb and route

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,12 @@
 class TeamsController < ApplicationController
   get '/account' do
+    redirect_to_login
     @teams = Team.all
     erb :'/teams/show'
+  end
+
+  get '/teams/new' do
+    redirect_to_login
+    erb :'teams/new'
   end
 end
