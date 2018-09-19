86fb921944c16a08befacbf7969bb3632154f478
Katrina Pritchard <pritckat@gmail.com>
2018-09-19 16:22:56 +0000

    can create teams as a user

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,11 +2,28 @@ class TeamsController < ApplicationController
   get '/account' do
     redirect_to_login
     @teams = Team.all
-    erb :'/teams/show'
+    erb :'/users/show'
   end
 
   get '/teams/new' do
     redirect_to_login
     erb :'teams/new'
   end
+
+  post '/teams/new' do
+    team = Team.new(params[:team])
+    team.user_id = current_user.id
+    team.save
+    redirect :"/teams/#{team.id}"
+  end
+
+  get '/teams/:id' do
+    @team = Team.find(params[:id])
+    erb :'teams/show'
+  end
+
+  get '/teams/:id/edit' do
+
+  end
+
 end
