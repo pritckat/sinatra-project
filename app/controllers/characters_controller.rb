4a4b935fd1977321c98b492ed63be593b8ff7a34
Katrina Pritchard <pritckat@gmail.com>
2018-09-19 17:41:24 +0000

    delete character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -26,4 +26,15 @@ class CharactersController < ApplicationController
     @char.save
     redirect to "/account"
   end
+
+  get '/characters/:id/delete' do
+    @char = Character.find_by_id(params[:id])
+    erb :'/characters/delete'
+  end
+
+  delete '/characters/:id/delete' do
+    @char = Character.find_by_id(params[:id])
+    @char.destroy
+    redirect to "/account"
+  end
 end
