8339e630deeb407663638d23a531dc29802700ed
Katrina Pritchard <pritckat@gmail.com>
2018-09-19 00:23:59 +0000

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :'/users/new'
   end
+
+  post '/signup' do
+    user = User.create(params[:user])
+    redirect to "/"
+  end
 end
