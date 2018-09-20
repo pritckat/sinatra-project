c3aa9e2f3d81a3e00705c5e5c5abc92be16f5b06
Katrina Pritchard <pritckat@gmail.com>
2018-09-20 18:15:49 +0000

    Add team composition method to teams

--- app/models/teams.rb
+++ app/models/teams.rb
@@ -2,4 +2,11 @@ class Team < ActiveRecord::Base
   belongs_to :user
   has_many :team_characters
   has_many :characters, through: :team_characters
+
+  def team_composition
+    support = self.characters.support.count
+    tank = self.characters.tank.count
+    damage = self.characters.damage.count
+    return "(#{support} - #{tank} - #{damage})"
+  end
 end
